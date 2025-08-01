4cfc578af710710bf2716aa85173ac7c7b270e8e
KevinGayda <kevingayda@gmail.com>
2025-08-01 14:55:06 -0700

    commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,56 +1,64 @@
 //html element selections for edit
-const editProfileBtn = document.querySelector(".profile__edit-btn");//class on line 22
-const editProfileModal = document.querySelector("#edit-profile-modal");//id on line 87
+const editProfileBtn = document.querySelector(".profile__edit-btn"); //class on line 22
+const editProfileModal = document.querySelector("#edit-profile-modal"); //id on line 87
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
 const editProfileNameInput = editProfileModal.querySelector("#profile__name-input");
 const editProfileDescriptionInput = editProfileModal.querySelector("#profile__description-input");
-const editProfileForm = editProfileModal.querySelector(".modal__form");// class on lines 91 and 109
+const editProfileForm = editProfileModal.querySelector(".modal__form"); // class on lines 91 and 109
 
 //html element selections for new
 const newPostBtn = document.querySelector(".profile__add-btn");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
-
 const profileName = document.querySelector(".profile__name");
 const profileDescription = document.querySelector(".profile__description");
+const newPostDescriptionInput = document.querySelector("#new__profile__description-input");
+const newPostImageInput = document.querySelector("#new__profile__name-input");
+
+//functions for repeats
+function closeModal(modal) {
+  modal.classList.remove("modal__is-opened");
+}
+
+function openModal(modal_2) {
+  modal_2.classList.add("modal__is-opened");
+}
 
 //This ensures that the edit post form will pop up when the edit post button is clicked
-editProfileBtn.addEventListener("click", function(){
-  editProfileNameInput.value = profileName.textContent;   //This line of code ensures that the textbox matches up with the name of the person
-  editProfileDescriptionInput.value = profileDescription.textContent;    //This line of code ensures that the textbox matches up with the person's occupation(Did this one on my own.)
-  editProfileModal.classList.add("modal__is-opened");
+editProfileBtn.addEventListener("click", function () {
+  editProfileNameInput.value = profileName.textContent; //This line of code ensures that the textbox matches up with the name of the person
+  editProfileDescriptionInput.value = profileDescription.textContent; //This line of code ensures that the textbox matches up with the person's occupation(Did this one on my own.)
+  openModal(editProfileModal);
 });
 
 //This ensures that the close button in the edit profile form will work properly
-editProfileCloseBtn.addEventListener("click", function(){
-  editProfileModal.classList.remove("modal__is-opened");
+editProfileCloseBtn.addEventListener("click", function () {
+  closeModal(editProfileModal);
 });
 
 // This ensures that the new post form will pop up when the new post button is clicked
-newPostBtn.addEventListener("click", function(){
-  newPostModal.classList.add("modal__is-opened");
+newPostBtn.addEventListener("click", function () {
+  openModal(newPostModal);
 });
 
 //This ensures that the close button in the new post form will work properly
-newPostCloseBtn.addEventListener("click", function(){
-  newPostModal.classList.remove("modal__is-opened");
+newPostCloseBtn.addEventListener("click", function () {
+  closeModal(newPostModal);
 });
 
-
-function handelEditProfileSubmit(evt){
+function handelEditProfileSubmit(evt) {
   evt.preventDefault();
   profileName.textContent = editProfileNameInput.value;
   profileDescription.textContent = editProfileDescriptionInput.value;
-  editProfileModal.classList.remove("modal__is-opened");
+  closeModal(editProfileModal);
 }
 
-function handleAddCardSubmit(evt){
+function handleAddCardSubmit(evt) {
   evt.preventDefault();
-
-
-  editProfileModal.classList.remove("modal__is-opened");
+  console.log(newPostImageInput.value);
+  console.log(newPostDescriptionInput.value);
+  closeModal(newPostModal);
 }
 
 editProfileForm.addEventListener("submit", handelEditProfileSubmit);
-
-
+newPostModal.addEventListener("submit", handleAddCardSubmit);
